ad2bde56306f075daec252e1eefb972d28d878c4
Andrew Nikonchuk <anikonch@gmail.com>
2018-08-13 19:57:29 +0000

    automatically backed up by learn

--- app.rb
+++ app.rb
@@ -11,4 +11,9 @@ class App < Sinatra::Base
     erb :create_puppy
   end
 
+  post '/display_puppy' do
+    @puppy = Puppy.new(params[:name], params[:breed], params[:age])
+    erb :display_puppy
+  end
+
 end
